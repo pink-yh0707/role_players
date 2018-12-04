d524368c0f3fdcbb637bb2115916a55e958610f2
日野勇進 <yushinohino@Pinkid.local>
2018-12-05 01:27:17 +0900

    Userモデルにnameカラムを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_params, if: :devise_controller?
+
+  private
+    def configure_permitted_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_093007) do
+ActiveRecord::Schema.define(version: 2018_12_04_160822) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,7 +24,9 @@ ActiveRecord::Schema.define(version: 2018_12_04_093007) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
