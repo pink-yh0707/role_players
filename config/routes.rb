fc1a978836d8861f44453f642333148ed18ee631
日野勇進 <yushinohino@Pinkid.local>
2018-12-07 17:54:54 +0900

    usersコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   root 'home#index'
-  devise_for :users
+  devise_for :users, :controllers => {
+    :sessions      => "users/sessions",
+    :registrations => "users/registrations",
+    :passwords     => "users/passwords"
+  }
+  resources :users, only: [:index, :show]
 end
