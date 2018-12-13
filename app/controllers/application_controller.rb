042c23d713bf710d9582e0cf7d59474c53b74a9d
日野勇進 <yushinohino@Pinkid.local>
2018-12-14 01:16:36 +0900

    ユーザー編集ページ仮作成及びパスワードを入力せずとも更新できるよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     # deviseでuser_nameカラム追加時の設定
     def configure_permitted_params
       devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
-    end
+    end 
 
     # deviseでのログイン後の遷移先の設定
     def after_sign_in_path_for(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  attr_accessor :current_password
+
   # 8~32文字内で半角英数字が1つ以上含まれている
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
@@ -12,4 +14,19 @@ class User < ApplicationRecord
   validates :user_name, presence: true, uniqueness: true
 
   mount_uploader :image, ImageUploader
+
+    def update_without_current_password(params, *options)
+      # edit機能を使うためにユーザーのパスワードを入力するというdeviseの使用を変更するため
+      params.delete(current_password)
+
+      # パスワード、確認パスワードが両方入力されているときのみ更新できるようにするため
+      if params[:password].blank? && params[:password_confirmation].blank?
+        params[:password].delete
+        params[:password_confirmation].delete
+      end
+
+      result = update_attributes(params, *options)
+      clean_up_passwords
+      result
+    end
 end
