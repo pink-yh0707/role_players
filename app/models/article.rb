07fbd5f9c61637ae3b03e62e081e509ecc8abb71
日野勇進 <yushinohino@Pinkid.local>
2018-12-20 23:52:01 +0900

    ヘッダー及び記事一覧ページのデザイン修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,10 @@
 class Article < ApplicationRecord
   belongs_to :user
 
-  has_one :player_image, dependent: :destroy
-  accepts_nested_attributes_for :player_image, update_only: true
-
   has_one :player, dependent: :destroy
   accepts_nested_attributes_for :player, update_only: true
+
+  default_scope -> { order(created_at: :desc) }
 
   validates :user_id, presence: true
   validates :article_title, presence: true, length: { maximum: 50 }
