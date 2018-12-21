d617a9e22491c0ba8337a937b5d73c484593f357
日野勇進 <yushinohino@Pinkid.local>
2018-12-21 20:26:43 +0900

    記事詳細ページのデザイン、及びその他ページデザインの微調整

--- app/uploaders/article_image_uploader.rb
+++ app/uploaders/article_image_uploader.rb
@@ -15,7 +15,7 @@ class ArticleImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    "article_image.jpeg"
+    "article_image.jpg"
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
