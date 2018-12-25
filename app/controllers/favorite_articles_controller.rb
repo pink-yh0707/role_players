c3de42d740c40f7acbc4db7ccb783e490dab3931
日野勇進 <yushinohino@Pinkid.local>
2018-12-25 20:26:17 +0900

    ajaxで非同期でいいね機能が使えるよう修正

--- app/controllers/favorite_articles_controller.rb
+++ app/controllers/favorite_articles_controller.rb
@@ -2,19 +2,21 @@ class FavoriteArticlesController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @article = Article.find(params[:article_id])
     current_user.favorite_articles.create(article_id: params[:article_id])
     respond_to do |format|
       format.html { redirect_to request.referrer || root_path }
-      format.json
+      format.js
     end
   end
 
   def destroy
+    @article = Article.find(params[:article_id])
     @favorite_article = FavoriteArticle.find_by(user_id: params[:id], article_id: params[:article_id])
     @favorite_article.destroy
     respond_to do |format|
       format.html { redirect_to request.referrer || root_path }
-      format.json
+      format.js
     end
   end
 end
