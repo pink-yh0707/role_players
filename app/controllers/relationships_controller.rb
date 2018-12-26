94262db5de21a021fadfc7ee95318864071a930b
日野勇進 <yushinohino@Pinkid.local>
2018-12-26 15:53:59 +0900

    フォロー機能を仮実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,12 @@
 class RelationshipsController < ApplicationController
   def create
+    current_user.active_relationships.create(followed_id: params[:user_id])
+    redirect_to request.referrer || root_url
   end
 
   def destroy
-  end 
+    relationship  = current_user.active_relationships.find_by(followed_id: params[:user_id])
+    relationship.destroy
+    redirect_to request.referrer || root_url
+  end
 end
