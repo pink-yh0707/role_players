4bc0691b9f2dd7bcdcc5ef844d4a3ed66e70598f
日野勇進 <yushinohino@Pinkid.local>
2018-12-27 14:45:13 +0900

    フォロー時非同期でフォロワーのカウントが反映されるよう修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,7 @@
 class RelationshipsController < ApplicationController
   def create
-    @user = (params[:user_id])
-    current_user.active_relationships.create(followed_id: @user)
+    @user = User.find(params[:user_id])
+    current_user.active_relationships.create(followed_id: params[:user_id])
     respond_to do |format|
       format.html { redirect_to request.referrer || root_url }
       format.js
@@ -9,8 +9,8 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
-    @user = (params[:user_id])
-    @relationship  = current_user.active_relationships.find_by(followed_id: @user)
+    @user = User.find(params[:user_id])
+    @relationship  = current_user.active_relationships.find_by(followed_id: params[:user_id])
     @relationship.destroy
     respond_to do |format|
       format.html { redirect_to request.referrer || root_url }
