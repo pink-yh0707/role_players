51f627f2cbd24046a37642fdc508f342cce9190b
日野勇進 <yushinohino@Pinkid.local>
2019-01-06 22:28:31 +0900

    Usersコントローラーの修正とshow_follow.html.erbの一部をパーシャル化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,16 +11,12 @@ class UsersController < ApplicationController
   end
 
   def following
-    @title = "Following"
-    @title_users = "フォロー"
     @user = User.find(params[:id])
     @users = @user.following
     render "show_follow"
   end
 
   def followers
-    @title = "Followers"
-    @title_users = "フォロワー"
     @user = User.find(params[:id])
     @users = @user.followers
     render "show_follow"
