dd09b4772751ea06660770ee59d05a55cbf5508a
日野勇進 <yushinohino@Pinkid.local>
2019-01-10 20:08:02 +0900

    Articlesコントローラーテスト途中経過

---
+++ spec/factories/articles.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :article do
+    article_title "記事タイトル"
+    content "紹介文"
+
+    association :player, factory: :player,
+      player_name: "プレイヤー",
+      team_name: "チーム",
+      country: "国籍",
+      position: "ポジション",
+      height: "177",
+      weight: "75"
+  end
+end
