d77f334eb9317cc1bcdd6ac27dc4813679726bbe
日野勇進 <yushinohino@Pinkid.local>
2019-01-28 22:04:47 +0900

    Homeコントローラーのindexアクションを修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       @user = User.find(current_user.id)
-      @articles = @user.articles.all
+      @articles = @user.articles.page(params[:page]).per(5)
     end
   end
 end
