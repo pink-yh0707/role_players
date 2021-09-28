95ca8e39e02c3ccc16fcbb06faf04bf2d35b3054
pink-yh0707 <slash.a77@gmail.com>
2021-09-28 14:27:42 +0900

    SendGrid仕様変更による対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name            => ENV["SENDGRID_USERNAME"],
-    :password             => ENV["SENDGRID_PASSWORD"],
+    :user_name            => 'apikey',
+    :password             => ENV["SENDGRID_APIKEY"],
     :domain               => "heroku.com",
     :addresses            => "smtp.sendgrid.net",
     :port                 => "587",
